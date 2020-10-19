42fd13b61613df33a396f64b421f97ea744943ba
tashaxing <tashaxing123@163.com>
2020-10-19 18:18:58 +0800

    init

---
+++ http_server.cpp
@@ -0,0 +1,804 @@
+#include "common/xlogger.hpp"
+#include "http_server.h"
+
+namespace http
+{
+	namespace server
+	{
+		namespace mime_types
+		{
+
+			struct mapping
+			{
+				const char* extension;
+				const char* mime_type;
+			} mappings[] =
+			{
+			  { "gif", "image/gif" },
+			  { "htm", "text/html" },
+			  { "html", "text/html" },
+			  { "jpg", "image/jpeg" },
+			  { "png", "image/png" }
+			};
+
+			std::string extension_to_type(const std::string& extension)
+			{
+				for (mapping m : mappings)
+				{
+					if (m.extension == extension)
+					{
+						return m.mime_type;
+					}
+				}
+
+				return "text/plain";
+			}
+
+		} // namespace mime_types
+
+
+		namespace status_strings
+		{
+
+			const std::string ok =
+				"HTTP/1.0 200 OK\r\n";
+			const std::string created =
+				"HTTP/1.0 201 Created\r\n";
+			const std::string accepted =
+				"HTTP/1.0 202 Accepted\r\n";
+			const std::string no_content =
+				"HTTP/1.0 204 No Content\r\n";
+			const std::string multiple_choices =
+				"HTTP/1.0 300 Multiple Choices\r\n";
+			const std::string moved_permanently =
+				"HTTP/1.0 301 Moved Permanently\r\n";
+			const std::string moved_temporarily =
+				"HTTP/1.0 302 Moved Temporarily\r\n";
+			const std::string not_modified =
+				"HTTP/1.0 304 Not Modified\r\n";
+			const std::string bad_request =
+				"HTTP/1.0 400 Bad Request\r\n";
+			const std::string unauthorized =
+				"HTTP/1.0 401 Unauthorized\r\n";
+			const std::string forbidden =
+				"HTTP/1.0 403 Forbidden\r\n";
+			const std::string not_found =
+				"HTTP/1.0 404 Not Found\r\n";
+			const std::string internal_server_error =
+				"HTTP/1.0 500 Internal Server Error\r\n";
+			const std::string not_implemented =
+				"HTTP/1.0 501 Not Implemented\r\n";
+			const std::string bad_gateway =
+				"HTTP/1.0 502 Bad Gateway\r\n";
+			const std::string service_unavailable =
+				"HTTP/1.0 503 Service Unavailable\r\n";
+
+			asio::const_buffer to_buffer(HttpReply::status_type status)
+			{
+				switch (status)
+				{
+				case HttpReply::ok:
+					return asio::buffer(ok);
+				case HttpReply::created:
+					return asio::buffer(created);
+				case HttpReply::accepted:
+					return asio::buffer(accepted);
+				case HttpReply::no_content:
+					return asio::buffer(no_content);
+				case HttpReply::multiple_choices:
+					return asio::buffer(multiple_choices);
+				case HttpReply::moved_permanently:
+					return asio::buffer(moved_permanently);
+				case HttpReply::moved_temporarily:
+					return asio::buffer(moved_temporarily);
+				case HttpReply::not_modified:
+					return asio::buffer(not_modified);
+				case HttpReply::bad_request:
+					return asio::buffer(bad_request);
+				case HttpReply::unauthorized:
+					return asio::buffer(unauthorized);
+				case HttpReply::forbidden:
+					return asio::buffer(forbidden);
+				case HttpReply::not_found:
+					return asio::buffer(not_found);
+				case HttpReply::internal_server_error:
+					return asio::buffer(internal_server_error);
+				case HttpReply::not_implemented:
+					return asio::buffer(not_implemented);
+				case HttpReply::bad_gateway:
+					return asio::buffer(bad_gateway);
+				case HttpReply::service_unavailable:
+					return asio::buffer(service_unavailable);
+				default:
+					return asio::buffer(internal_server_error);
+				}
+			}
+
+		} // namespace status_strings
+
+		namespace misc_strings
+		{
+
+			const char name_value_separator[] = { ':', ' ' };
+			const char crlf[] = { '\r', '\n' };
+
+		} // namespace misc_strings
+
+		std::vector<asio::const_buffer> HttpReply::to_buffers()
+		{
+			std::vector<asio::const_buffer> buffers;
+			buffers.push_back(status_strings::to_buffer(status));
+			for (std::size_t i = 0; i < headers.size(); ++i)
+			{
+				HttpHeader& h = headers[i];
+				buffers.push_back(asio::buffer(h.name));
+				buffers.push_back(asio::buffer(misc_strings::name_value_separator));
+				buffers.push_back(asio::buffer(h.value));
+				buffers.push_back(asio::buffer(misc_strings::crlf));
+			}
+			buffers.push_back(asio::buffer(misc_strings::crlf));
+			buffers.push_back(asio::buffer(content));
+			return buffers;
+		}
+
+		namespace routine_replies
+		{
+
+			const char ok[] = "";
+			const char created[] =
+				"<html>"
+				"<head><title>Created</title></head>"
+				"<body><h1>201 Created</h1></body>"
+				"</html>";
+			const char accepted[] =
+				"<html>"
+				"<head><title>Accepted</title></head>"
+				"<body><h1>202 Accepted</h1></body>"
+				"</html>";
+			const char no_content[] =
+				"<html>"
+				"<head><title>No Content</title></head>"
+				"<body><h1>204 Content</h1></body>"
+				"</html>";
+			const char multiple_choices[] =
+				"<html>"
+				"<head><title>Multiple Choices</title></head>"
+				"<body><h1>300 Multiple Choices</h1></body>"
+				"</html>";
+			const char moved_permanently[] =
+				"<html>"
+				"<head><title>Moved Permanently</title></head>"
+				"<body><h1>301 Moved Permanently</h1></body>"
+				"</html>";
+			const char moved_temporarily[] =
+				"<html>"
+				"<head><title>Moved Temporarily</title></head>"
+				"<body><h1>302 Moved Temporarily</h1></body>"
+				"</html>";
+			const char not_modified[] =
+				"<html>"
+				"<head><title>Not Modified</title></head>"
+				"<body><h1>304 Not Modified</h1></body>"
+				"</html>";
+			const char bad_request[] =
+				"<html>"
+				"<head><title>Bad Request</title></head>"
+				"<body><h1>400 Bad Request</h1></body>"
+				"</html>";
+			const char unauthorized[] =
+				"<html>"
+				"<head><title>Unauthorized</title></head>"
+				"<body><h1>401 Unauthorized</h1></body>"
+				"</html>";
+			const char forbidden[] =
+				"<html>"
+				"<head><title>Forbidden</title></head>"
+				"<body><h1>403 Forbidden</h1></body>"
+				"</html>";
+			const char not_found[] =
+				"<html>"
+				"<head><title>Not Found</title></head>"
+				"<body><h1>404 Not Found</h1></body>"
+				"</html>";
+			const char internal_server_error[] =
+				"<html>"
+				"<head><title>Internal Server Error</title></head>"
+				"<body><h1>500 Internal Server Error</h1></body>"
+				"</html>";
+			const char not_implemented[] =
+				"<html>"
+				"<head><title>Not Implemented</title></head>"
+				"<body><h1>501 Not Implemented</h1></body>"
+				"</html>";
+			const char bad_gateway[] =
+				"<html>"
+				"<head><title>Bad Gateway</title></head>"
+				"<body><h1>502 Bad Gateway</h1></body>"
+				"</html>";
+			const char service_unavailable[] =
+				"<html>"
+				"<head><title>Service Unavailable</title></head>"
+				"<body><h1>503 Service Unavailable</h1></body>"
+				"</html>";
+
+			std::string to_string(HttpReply::status_type status)
+			{
+				switch (status)
+				{
+				case HttpReply::ok:
+					return ok;
+				case HttpReply::created:
+					return created;
+				case HttpReply::accepted:
+					return accepted;
+				case HttpReply::no_content:
+					return no_content;
+				case HttpReply::multiple_choices:
+					return multiple_choices;
+				case HttpReply::moved_permanently:
+					return moved_permanently;
+				case HttpReply::moved_temporarily:
+					return moved_temporarily;
+				case HttpReply::not_modified:
+					return not_modified;
+				case HttpReply::bad_request:
+					return bad_request;
+				case HttpReply::unauthorized:
+					return unauthorized;
+				case HttpReply::forbidden:
+					return forbidden;
+				case HttpReply::not_found:
+					return not_found;
+				case HttpReply::internal_server_error:
+					return internal_server_error;
+				case HttpReply::not_implemented:
+					return not_implemented;
+				case HttpReply::bad_gateway:
+					return bad_gateway;
+				case HttpReply::service_unavailable:
+					return service_unavailable;
+				default:
+					return internal_server_error;
+				}
+			}
+
+		} // namespace routine_replies
+
+		HttpReply HttpReply::routine_reply(HttpReply::status_type status)
+		{
+			HttpReply rep;
+			rep.status = status;
+			rep.content = routine_replies::to_string(status);
+			rep.headers.resize(2);
+			rep.headers[0].name = "Content-Length";
+			rep.headers[0].value = std::to_string(rep.content.size());
+			rep.headers[1].name = "Content-Type";
+			rep.headers[1].value = mime_types::extension_to_type("json");
+			return rep;
+		}
+
+		HttpRequestParser::HttpRequestParser()
+			: state_(method_start)
+		{}
+
+		void HttpRequestParser::reset()
+		{
+			state_ = method_start;
+		}
+
+		HttpRequestParser::result_type HttpRequestParser::consume(HttpRequest& req, char input)
+		{
+			switch (state_)
+			{
+			case method_start:
+				if (!is_char(input) || is_ctl(input) || is_tspecial(input))
+				{
+					return bad;
+				}
+				else
+				{
+					state_ = method;
+					req.method.push_back(input);
+					return indeterminate;
+				}
+			case method:
+				if (input == ' ')
+				{
+					state_ = uri;
+					return indeterminate;
+				}
+				else if (!is_char(input) || is_ctl(input) || is_tspecial(input))
+				{
+					return bad;
+				}
+				else
+				{
+					req.method.push_back(input);
+					return indeterminate;
+				}
+			case uri:
+				if (input == ' ')
+				{
+					state_ = http_version_h;
+					return indeterminate;
+				}
+				else if (is_ctl(input))
+				{
+					return bad;
+				}
+				else
+				{
+					req.uri.push_back(input);
+					return indeterminate;
+				}
+			case http_version_h:
+				if (input == 'H')
+				{
+					state_ = http_version_t_1;
+					return indeterminate;
+				}
+				else
+				{
+					return bad;
+				}
+			case http_version_t_1:
+				if (input == 'T')
+				{
+					state_ = http_version_t_2;
+					return indeterminate;
+				}
+				else
+				{
+					return bad;
+				}
+			case http_version_t_2:
+				if (input == 'T')
+				{
+					state_ = http_version_p;
+					return indeterminate;
+				}
+				else
+				{
+					return bad;
+				}
+			case http_version_p:
+				if (input == 'P')
+				{
+					state_ = http_version_slash;
+					return indeterminate;
+				}
+				else
+				{
+					return bad;
+				}
+			case http_version_slash:
+				if (input == '/')
+				{
+					req.http_version_major = 0;
+					req.http_version_minor = 0;
+					state_ = http_version_major_start;
+					return indeterminate;
+				}
+				else
+				{
+					return bad;
+				}
+			case http_version_major_start:
+				if (is_digit(input))
+				{
+					req.http_version_major = req.http_version_major * 10 + input - '0';
+					state_ = http_version_major;
+					return indeterminate;
+				}
+				else
+				{
+					return bad;
+				}
+			case http_version_major:
+				if (input == '.')
+				{
+					state_ = http_version_minor_start;
+					return indeterminate;
+				}
+				else if (is_digit(input))
+				{
+					req.http_version_major = req.http_version_major * 10 + input - '0';
+					return indeterminate;
+				}
+				else
+				{
+					return bad;
+				}
+			case http_version_minor_start:
+				if (is_digit(input))
+				{
+					req.http_version_minor = req.http_version_minor * 10 + input - '0';
+					state_ = http_version_minor;
+					return indeterminate;
+				}
+				else
+				{
+					return bad;
+				}
+			case http_version_minor:
+				if (input == '\r')
+				{
+					state_ = expecting_newline_1;
+					return indeterminate;
+				}
+				else if (is_digit(input))
+				{
+					req.http_version_minor = req.http_version_minor * 10 + input - '0';
+					return indeterminate;
+				}
+				else
+				{
+					return bad;
+				}
+			case expecting_newline_1:
+				if (input == '\n')
+				{
+					state_ = header_line_start;
+					return indeterminate;
+				}
+				else
+				{
+					return bad;
+				}
+			case header_line_start:
+				if (input == '\r')
+				{
+					state_ = expecting_newline_3;
+					return indeterminate;
+				}
+				else if (!req.headers.empty() && (input == ' ' || input == '\t'))
+				{
+					state_ = header_lws;
+					return indeterminate;
+				}
+				else if (!is_char(input) || is_ctl(input) || is_tspecial(input))
+				{
+					return bad;
+				}
+				else
+				{
+					req.headers.push_back(HttpHeader());
+					req.headers.back().name.push_back(input);
+					state_ = header_name;
+					return indeterminate;
+				}
+			case header_lws:
+				if (input == '\r')
+				{
+					state_ = expecting_newline_2;
+					return indeterminate;
+				}
+				else if (input == ' ' || input == '\t')
+				{
+					return indeterminate;
+				}
+				else if (is_ctl(input))
+				{
+					return bad;
+				}
+				else
+				{
+					state_ = header_value;
+					req.headers.back().value.push_back(input);
+					return indeterminate;
+				}
+			case header_name:
+				if (input == ':')
+				{
+					state_ = space_before_header_value;
+					return indeterminate;
+				}
+				else if (!is_char(input) || is_ctl(input) || is_tspecial(input))
+				{
+					return bad;
+				}
+				else
+				{
+					req.headers.back().name.push_back(input);
+					return indeterminate;
+				}
+			case space_before_header_value:
+				if (input == ' ')
+				{
+					state_ = header_value;
+					return indeterminate;
+				}
+				else
+				{
+					return bad;
+				}
+			case header_value:
+				if (input == '\r')
+				{
+					state_ = expecting_newline_2;
+					return indeterminate;
+				}
+				else if (is_ctl(input))
+				{
+					return bad;
+				}
+				else
+				{
+					req.headers.back().value.push_back(input);
+					return indeterminate;
+				}
+			case expecting_newline_2:
+				if (input == '\n')
+				{
+					state_ = header_line_start;
+					return indeterminate;
+				}
+				else
+				{
+					return bad;
+				}
+			case expecting_newline_3:
+				return (input == '\n') ? good : bad;
+			default:
+				return bad;
+			}
+		}
+
+		bool HttpRequestParser::is_char(int c)
+		{
+			return c >= 0 && c <= 127;
+		}
+
+		bool HttpRequestParser::is_ctl(int c)
+		{
+			return (c >= 0 && c <= 31) || (c == 127);
+		}
+
+		bool HttpRequestParser::is_tspecial(int c)
+		{
+			switch (c)
+			{
+			case '(': case ')': case '<': case '>': case '@':
+			case ',': case ';': case ':': case '\\': case '"':
+			case '/': case '[': case ']': case '?': case '=':
+			case '{': case '}': case ' ': case '\t':
+				return true;
+			default:
+				return false;
+			}
+		}
+
+		bool HttpRequestParser::is_digit(int c)
+		{
+			return c >= '0' && c <= '9';
+		}
+
+		HttpServerCallback::HttpServerCallback()
+		{}
+
+		void HttpServerCallback::handle_request(const HttpRequest& req, HttpReply& rep)
+		{
+		}
+
+		void HttpServerCallback::compose_response(HttpReply& rep, HttpReply::status_type status, std::string& rsp)
+		{
+			rep.status = status;
+			rep.content = rsp;
+
+			rep.headers.resize(2);
+			rep.headers[0].name = "Content-Length";
+			rep.headers[0].value = std::to_string(rep.content.size());
+			rep.headers[1].name = "Content-Type";
+			rep.headers[1].value = mime_types::extension_to_type("json");
+		}
+
+		bool HttpServerCallback::url_decode(const std::string& in, std::string& out)
+		{
+			out.clear();
+			out.reserve(in.size());
+			for (std::size_t i = 0; i < in.size(); ++i)
+			{
+				if (in[i] == '%')
+				{
+					if (i + 3 <= in.size())
+					{
+						int value = 0;
+						std::istringstream is(in.substr(i + 1, 2));
+						if (is >> std::hex >> value)
+						{
+							out += static_cast<char>(value);
+							i += 2;
+						}
+						else
+						{
+							return false;
+						}
+					}
+					else
+					{
+						return false;
+					}
+				}
+				else if (in[i] == '+')
+				{
+					out += ' ';
+				}
+				else
+				{
+					out += in[i];
+				}
+			}
+			return true;
+		}
+
+		HttpConnectionManager::HttpConnectionManager()
+		{}
+
+		void HttpConnectionManager::start(connection_ptr c)
+		{
+			connections_.insert(c);
+			c->start();
+		}
+
+		void HttpConnectionManager::stop(connection_ptr c)
+		{
+			connections_.erase(c);
+			c->stop();
+		}
+
+		void HttpConnectionManager::stop_all()
+		{
+			for (auto c : connections_)
+				c->stop();
+			connections_.clear();
+		}
+
+		HttpConnection::HttpConnection(asio::ip::tcp::socket socket,
+			HttpConnectionManager& manager, HttpServerCallback* handler)
+			: socket_(std::move(socket)),
+			connection_manager_(manager),
+			request_handler_(handler)
+		{}
+
+		void HttpConnection::start()
+		{
+			do_read();
+		}
+
+		void HttpConnection::stop()
+		{
+			socket_.close();
+		}
+
+		void HttpConnection::do_read()
+		{
+			auto self(shared_from_this());
+			socket_.async_read_some(asio::buffer(buffer_),
+				[this, self](std::error_code ec, std::size_t bytes_transferred)
+				{
+					if (!ec)
+					{
+						HttpRequestParser::result_type result;
+						std::tie(result, std::ignore) = request_parser_.parse(
+							request_, buffer_.data(), buffer_.data() + bytes_transferred);
+
+						if (result == HttpRequestParser::good)
+						{
+							request_handler_->handle_request(request_, reply_);
+							do_write();
+						}
+						else if (result == HttpRequestParser::bad)
+						{
+							reply_ = HttpReply::routine_reply(HttpReply::bad_request);
+							do_write();
+						}
+						else
+						{
+							do_read();
+						}
+					}
+					else if (ec != asio::error::operation_aborted)
+					{
+						connection_manager_.stop(shared_from_this());
+					}
+				});
+		}
+
+		void HttpConnection::do_write()
+		{
+			auto self(shared_from_this());
+			asio::async_write(socket_, reply_.to_buffers(),
+				[this, self](std::error_code ec, std::size_t)
+				{
+					if (!ec)
+					{
+						// Initiate graceful connection closure.
+						asio::error_code ignored_ec;
+						socket_.shutdown(asio::ip::tcp::socket::shutdown_both,
+							ignored_ec);
+					}
+
+					if (ec != asio::error::operation_aborted)
+					{
+						connection_manager_.stop(shared_from_this());
+					}
+				});
+		}
+
+
+		HttpServer::HttpServer(int port):
+			io_context_(1),
+			signals_(io_context_),
+			acceptor_(io_context_),
+			connection_manager_()
+		{
+			m_port = port;
+
+			// Open the acceptor with the option to reuse the address (i.e. SO_REUSEADDR).
+			asio::ip::tcp::resolver resolver(io_context_);
+
+			asio::ip::tcp::endpoint endpoint = *resolver.resolve("0.0.0.0", std::to_string(m_port)).begin();
+			acceptor_.open(endpoint.protocol());
+			acceptor_.set_option(asio::ip::tcp::acceptor::reuse_address(true));
+			acceptor_.bind(endpoint);
+			acceptor_.listen();
+
+			do_accept();
+		}
+
+		HttpServer::~HttpServer()
+		{
+			m_main_thread.join();
+		}
+
+		void HttpServer::start()
+		{
+			// start http server in independent thread
+			m_main_thread = std::thread([&]
+				{
+					// The io_context::run() call will block until all asynchronous operations
+					// have finished. While the server is running, there is always at least one
+					// asynchronous operation outstanding: the asynchronous accept call waiting
+					// for new incoming connections.
+					io_context_.run();
+				});
+			XLOG_INFO("HttpServer http server is listening at port: {}", m_port);
+		}
+
+		void HttpServer::stop()
+		{
+			acceptor_.close();
+			connection_manager_.stop_all();
+			io_context_.stop();
+		}
+
+		void HttpServer::set_request_handler(HttpServerCallback* req_handler)
+		{
+			// acually set outside inherited handler
+			request_handler_ = req_handler;
+		}
+
+		void HttpServer::do_accept()
+		{
+			acceptor_.async_accept(
+				[this](std::error_code ec, asio::ip::tcp::socket socket)
+				{
+					// Check whether the server was stopped before this
+					// completion handler had a chance to run.
+					if (!acceptor_.is_open())
+					{
+						return;
+					}
+
+					if (!ec)
+					{
+						connection_manager_.start(std::make_shared<HttpConnection>(
+							std::move(socket), connection_manager_, request_handler_));
+					}
+
+					do_accept();
+				});
+		}
+
+	} // namespace server
+} // namespace http
---
+++ http_server.h
@@ -0,0 +1,302 @@
+#ifndef _HTTP_SERVER_H_
+#define _HTTP_SERVER_H_
+
+#include <string>
+#include <vector>
+#include <tuple>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <memory>
+#include <utility>
+#include "asio.hpp"
+
+// --- the server code is copied from asio http example
+namespace http
+{
+	namespace server
+	{
+		const std::string HTTP_GET = "GET";
+		const std::string HTTP_POST = "POST";
+
+		struct HttpHeader
+		{
+			std::string name;
+			std::string value;
+		};
+
+		namespace mime_types
+		{
+
+			/// Convert a file extension into a MIME type.
+			std::string extension_to_type(const std::string& extension);
+
+		} // namespace mime_types
+
+		/// A request received from a client.
+		struct HttpRequest
+		{
+			std::string method;
+			std::string uri;
+			int http_version_major;
+			int http_version_minor;
+			std::vector<HttpHeader> headers;
+		};
+
+		/// A reply to be sent to a client.
+		struct HttpReply
+		{
+			/// The status of the reply.
+			enum status_type
+			{
+				ok = 200,
+				created = 201,
+				accepted = 202,
+				no_content = 204,
+				multiple_choices = 300,
+				moved_permanently = 301,
+				moved_temporarily = 302,
+				not_modified = 304,
+				bad_request = 400,
+				unauthorized = 401,
+				forbidden = 403,
+				not_found = 404,
+				internal_server_error = 500,
+				not_implemented = 501,
+				bad_gateway = 502,
+				service_unavailable = 503
+			} status;
+
+			/// The headers to be included in the reply.
+			std::vector<HttpHeader> headers;
+
+			/// The content to be sent in the reply.
+			std::string content;
+
+			/// Convert the reply into a vector of buffers. The buffers do not own the
+			/// underlying memory blocks, therefore the reply object must remain valid and
+			/// not be changed until the write operation has completed.
+			std::vector<asio::const_buffer> to_buffers();
+
+			/// Get a stock reply.
+			static HttpReply routine_reply(status_type status);
+		};
+
+		/// Parser for incoming requests.
+		class HttpRequestParser
+		{
+		public:
+			/// Construct ready to parse the request method.
+			HttpRequestParser();
+
+			/// Reset to initial parser state.
+			void reset();
+
+			/// Result of parse.
+			enum result_type { good, bad, indeterminate };
+
+			/// Parse some data. The enum return value is good when a complete request has
+			/// been parsed, bad if the data is invalid, indeterminate when more data is
+			/// required. The InputIterator return value indicates how much of the input
+			/// has been consumed.
+			template <typename InputIterator>
+			std::tuple<result_type, InputIterator> parse(HttpRequest& req,
+				InputIterator begin, InputIterator end)
+			{
+				while (begin != end)
+				{
+					result_type result = consume(req, *begin++);
+					if (result == good || result == bad)
+						return std::make_tuple(result, begin);
+				}
+				return std::make_tuple(indeterminate, begin);
+			}
+
+		private:
+			/// Handle the next character of input.
+			result_type consume(HttpRequest& req, char input);
+
+			/// Check if a byte is an HTTP character.
+			static bool is_char(int c);
+
+			/// Check if a byte is an HTTP control character.
+			static bool is_ctl(int c);
+
+			/// Check if a byte is defined as an HTTP tspecial character.
+			static bool is_tspecial(int c);
+
+			/// Check if a byte is a digit.
+			static bool is_digit(int c);
+
+			/// The current state of the parser.
+			enum state
+			{
+				method_start,
+				method,
+				uri,
+				http_version_h,
+				http_version_t_1,
+				http_version_t_2,
+				http_version_p,
+				http_version_slash,
+				http_version_major_start,
+				http_version_major,
+				http_version_minor_start,
+				http_version_minor,
+				expecting_newline_1,
+				header_line_start,
+				header_lws,
+				header_name,
+				space_before_header_value,
+				header_value,
+				expecting_newline_2,
+				expecting_newline_3
+			} state_;
+		};
+
+
+		/// The common handler for all incoming requests.
+		class HttpServerCallback
+		{
+		public:
+			HttpServerCallback(const HttpServerCallback&) = delete;
+			HttpServerCallback& operator=(const HttpServerCallback&) = delete;
+
+			/// Construct with a directory containing files to be served.
+			explicit HttpServerCallback();
+
+			/// Handle a request and produce a reply.
+			virtual void handle_request(const HttpRequest& req, HttpReply& rep);
+
+		protected:
+			// common method to package the json reponse as http frame
+			void compose_response(HttpReply& rep, HttpReply::status_type status, std::string& rsp);
+			/// Perform URL-decoding on a string. Returns false if the encoding was
+			/// invalid.
+			static bool url_decode(const std::string& in, std::string& out);
+		};
+
+		class HttpConnectionManager;
+		/// Represents a single connection from a client.
+		class HttpConnection
+			: public std::enable_shared_from_this<HttpConnection>
+		{
+		public:
+			HttpConnection(const HttpConnection&) = delete;
+			HttpConnection& operator=(const HttpConnection&) = delete;
+
+			/// Construct a connection with the given socket.
+			explicit HttpConnection(asio::ip::tcp::socket socket,
+				HttpConnectionManager& manager, HttpServerCallback* handler);
+
+			/// Start the first asynchronous operation for the connection.
+			void start();
+
+			/// Stop all asynchronous operations associated with the connection.
+			void stop();
+
+		private:
+			/// Perform an asynchronous read operation.
+			void do_read();
+
+			/// Perform an asynchronous write operation.
+			void do_write();
+
+			/// Socket for the connection.
+			asio::ip::tcp::socket socket_;
+
+			/// The manager for this connection.
+			HttpConnectionManager& connection_manager_;
+
+			/// The handler used to process the incoming request.
+			HttpServerCallback* request_handler_;
+
+			/// Buffer for incoming data.
+			std::array<char, 8192> buffer_;
+
+			/// The incoming request.
+			HttpRequest request_;
+
+			/// The parser for the incoming request.
+			HttpRequestParser request_parser_;
+
+			/// The reply to be sent back to the client.
+			HttpReply reply_;
+		};
+
+		typedef std::shared_ptr<HttpConnection> connection_ptr;
+
+		/// Manages open connections so that they may be cleanly stopped when the server
+/// needs to shut down.
+		class HttpConnectionManager
+		{
+		public:
+			HttpConnectionManager(const HttpConnectionManager&) = delete;
+			HttpConnectionManager& operator=(const HttpConnectionManager&) = delete;
+
+			/// Construct a connection manager.
+			HttpConnectionManager();
+
+			/// Add the specified connection to the manager and start it.
+			void start(connection_ptr c);
+
+			/// Stop the specified connection.
+			void stop(connection_ptr c);
+
+			/// Stop all connections.
+			void stop_all();
+
+		private:
+			/// The managed connections.
+			std::unordered_set<connection_ptr> connections_;
+		};
+
+		class HttpServer
+		{
+		public:
+			HttpServer(const HttpServer&) = delete;
+			HttpServer& operator=(const HttpServer&) = delete;
+
+		public:
+			explicit HttpServer(int port);
+			virtual ~HttpServer();
+
+			void start();
+			void stop();
+
+			void set_request_handler(HttpServerCallback* req_handler);
+
+		private:
+			/// Perform an asynchronous accept operation.
+			void do_accept();
+
+			/// Wait for a request to stop the server.
+			void do_await_stop();
+
+			/// The io_context used to perform asynchronous operations.
+			asio::io_context io_context_;
+
+			/// The signal_set is used to register for process termination notifications.
+			asio::signal_set signals_;
+
+			/// Acceptor used to listen for incoming connections.
+			asio::ip::tcp::acceptor acceptor_;
+
+			/// The connection manager which owns all live connections.
+			HttpConnectionManager connection_manager_;
+
+			/// The handler for all incoming requests.
+			HttpServerCallback* request_handler_ = NULL;
+
+		private:
+			int m_port;
+			std::thread m_main_thread;
+		};
+
+	} // namespace server
+} // namespace http
+
+
+
+#endif // !_HTTP_SERVER_H_
